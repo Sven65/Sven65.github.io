d2415dde4493fe864d32b7c8a92560b78de43827
Mackan <Sven65@users.noreply.github.com>
2016-09-24 22:01:33 +0200

    Update main.js

--- Counter/main.js
+++ Counter/main.js
@@ -1,8 +1,14 @@
 let count = 0;
 let c = document.querySelector('#count');
 
+window.onload = () => {
+	count = localStorage.getItem('count') | 0;
+	c.innerHTML = count;
+}
+
 
 document.addEventListener("click", (e) => {
 	count++;
 	c.innerHTML = count;
+	localStorage.setItem('count', count);
 });
