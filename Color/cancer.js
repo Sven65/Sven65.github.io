d0b8e8cbde1e8d4beb078f54dccca80f6df5d8ec
Mackan <Sven65@users.noreply.github.com>
2016-12-16 15:46:50 +0100

    Create cancer.js

---
+++ Color/cancer.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let shouldRun = true;
+
+	setInterval(() => {
+		if(shouldRun){
+			let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+			document.getElementById("memes").innerHTML = color;
+			document.getElementById("body").style.backgroundColor = color;
+		}
+	}, 1500)
+
+	document.addEventListener('click', () => {
+		shouldRun = !shouldRun;
+		document.getElementById("state").innerHTML = shouldRun?'Running':'Paused'
+	})
+
+})
