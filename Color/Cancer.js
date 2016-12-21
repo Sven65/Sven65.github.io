b998f27c901f5f2d1d3be18d42431ee93fe2baf2
Mackan <mackan@discorddungeons.me>
2016-12-21 14:18:28 +0100

    color fixes

--- Color/Cancer.js
+++ Color/Cancer.js
@@ -1,13 +1,26 @@
+function getColor(){
+	return new Promise((resolve, reject) => {
+		let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+		if(/(^#?[0-9A-F]{6}$)|(^#?[0-9A-F]{3}$)/i.test(color)){
+			resolve(color);
+		}else{
+			resolve(getColor())
+		}
+	});
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	let shouldRun = true;
 
 	setInterval(() => {
 		if(shouldRun){
-			let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-			document.getElementById("color").innerHTML = color;
-			document.getElementById("body").style.backgroundColor = color;
-			document.getElementById("tcol").setAttribute('content', color);
-			document.getElementById("tcol2").setAttribute('content', color);
+			getColor().then((color) => {
+				document.getElementById("color").innerHTML = color;
+				document.getElementById("body").style.backgroundColor = color;
+				document.getElementById("tcol").setAttribute('content', color);
+				document.getElementById("tcol2").setAttribute('content', color);
+			})
 		}
 	}, 1500)
 
