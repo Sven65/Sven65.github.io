284ccb3d73acc7e7d7707e5c53d7972e1bb78360
Mackan <thormax5@gmail.com>
2017-05-22 08:21:54 +0200

    kms

--- Spinner/Spin.js
+++ Spinner/Spin.js
@@ -1,7 +1,18 @@
-var degCount  = 0;
+var degCount = 0;
+var spinstodo = 0;
+var spinTimer = 1;
 
 function spin(){
-
+	if(spinstodo > 0){
+		degCount+= 1+((spinTimer/100)*0.20);
+		document.querySelector("#spinner").style.transform = "rotate("+degCount+"deg)";
+		spinstodo--;
+	}
 }
 
-document.querySelector("#spinner").style.transform = "rotate(150deg)"
+document.addEventListener("click", function(){
+	spinstodo += 10000;
+	spinTimer += 0.05;
+});
+
+setInterval(spin, spinTimer);
