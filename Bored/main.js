1d540e155e679fdc29dd43a74995669acb5ae927
Mackan <thormax5@gmail.com>
2017-09-07 10:50:03 +0200

    smh

--- Bored/main.js
+++ Bored/main.js
@@ -79,7 +79,7 @@ getJson('./Things.json').then(things => {
 
 	localStorage.q = q
 
-	document.querySelector("#thing").innerHTML = quotes[q];
+	document.querySelector("#thing").innerHTML = things[q];
 
 	addClass(document.querySelector("#thing"), "show")
 })
